b92e1fec6aaddd099a235d88fe83a04fc5d6dffc
Matt Chan <hellomatt@MacBook-Pro-5.local>
2015-11-23 17:37:51 -0800

    + Messages.app from Send Message button

--- OneToOne/PairingViewController.swift
+++ OneToOne/PairingViewController.swift
@@ -8,15 +8,18 @@
 
 import UIKit
 import Parse
+import MessageUI
 
-class PairingViewController: UIViewController {
-
+class PairingViewController: UIViewController, MFMessageComposeViewControllerDelegate{
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        messageLabel.text = "Tell the recipient to enter Seabiscuit within the next 10:00 to pair."
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -25,15 +28,35 @@ class PairingViewController: UIViewController {
     @IBAction func didPressCancel(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
     }
-
+    @IBAction func didPressSendMessage(sender: AnyObject) {
+        if MFMessageComposeViewController.canSendText() {
+            let messageVC = MFMessageComposeViewController()
+            messageVC.messageComposeDelegate = self
+            messageVC.recipients = ["Enter tel-nr"]
+            messageVC.body = "Download 1:1 (url) and enter code 'Seabiscuit' to  pair with me!"
+            self.presentViewController(messageVC, animated: true, completion: nil)
+        } else {
+            print("User hasn't setup Messages.app")
+        }
+        
+    }
+    
+    func messageComposeViewController(controller: MFMessageComposeViewController!, didFinishWithResult result: MessageComposeResult) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
+
